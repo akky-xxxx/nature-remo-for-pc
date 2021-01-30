4a4bdcf3cac8f858af393fa0d2116c5bd61873ac
akky-xxxx <114.s.aki@gmail.com>
2021-01-31 01:13:54 +0900

    chore: change directories

--- src/electron-src/index.ts
+++ src/electron-src/index.ts
@@ -23,7 +23,7 @@ app.on('ready', async () => {
   const url = isDev
     ? 'http://localhost:8000/'
     : format({
-        pathname: join(__dirname, '../renderer/out/index.html'),
+        pathname: join(__dirname, '../src/renderer/out/index.html'),
         protocol: 'file:',
         slashes: true,
       })
