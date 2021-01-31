1b7103e5fed7a5dfc94e1427b0bd2d93a8708d8f
akky-xxxx <114.s.aki@gmail.com>
2021-01-31 22:29:16 +0900

    refactor: remove unnecessary files and types

---
+++ src/renderer/@types/index.ts
@@ -0,0 +1,10 @@
+import { IpcRenderer } from "electron"
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      ipcRenderer: IpcRenderer
+    }
+  }
+}
--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react"
 import Link from "next/link"
-import Layout from "../components/Layout"
 
 const IndexPage = () => {
   useEffect(() => {
@@ -16,7 +15,7 @@ const IndexPage = () => {
   }
 
   return (
-    <Layout title="Home | Next.js + TypeScript + Electron Example">
+    <div>
       <h1>Hello Next.js 👋</h1>
       <button type="button" onClick={onSayHiClick}>
         Say hi to electron
@@ -24,7 +23,7 @@ const IndexPage = () => {
       <p>
         <Link href="/about">About</Link>
       </p>
-    </Layout>
+    </div>
   )
 }
 
