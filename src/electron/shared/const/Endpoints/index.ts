71d7681bfa99679f1fd684eb2dbee8a7a5020053
akky-xxxx <114.s.aki@gmail.com>
2021-02-07 17:05:14 +0900

    feat: define api of post signals signal

--- src/electron/shared/const/Endpoints/index.ts
+++ src/electron/shared/const/Endpoints/index.ts
@@ -2,4 +2,5 @@ export const Endpoints = {
   GET_APPLIANCES: "/1/appliances",
   POST_APPLIANCES_APPLIANCE: "/1/appliances/{:applianceId}",
   POST_APPLIANCE_ORDER: "/1/appliance_orders",
+  POST_SIGNALS_SIGNAL: "/1/signals/{:signalId}",
 } as const
